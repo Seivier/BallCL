25e23e6c1bfff0980006688a0f421c7e284c7383
Vicente González <vicentegonzalez@ug.uchile.cl>
2024-05-23 21:40:51 -0400

    fix: minor visualization

--- src/main.cc
+++ src/main.cc
@@ -15,8 +15,8 @@
 #include "imgui_impl_opengl3.h"
 #include "shader.h"
 
-#define WIDTH 800
-#define HEIGHT 800
+#define WIDTH 400
+#define HEIGHT 400
 #define N 100
 
 
@@ -220,19 +220,6 @@ int main(int, char**) {
 
       ImGui::End();
     }
-
-    // // 3. Show another simple window.
-    // if (show_another_window) {
-    //   ImGui::Begin(
-    //       "Another Window",
-    //       &show_another_window);  // Pass a pointer to our bool variable (the
-    //                               // window will have a closing button that
-    //                               will
-    //                               // clear the bool when clicked)
-    //   ImGui::Text("Hello from another window!");
-    //   if (ImGui::Button("Close Me")) show_another_window = false;
-    //   ImGui::End();
-    // }
 
     // Rendering
 
